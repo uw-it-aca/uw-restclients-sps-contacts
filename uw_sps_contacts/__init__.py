9fe2002ac6b22aa2555a8e3aeead8dc7604c1dad
Craig M. Stimmel <cstimmel@uw.edu>
2025-11-06 16:57:50 -0800

    Think the DAO is good here, but unproven

--- uw_sps_contacts/__init__.py
+++ uw_sps_contacts/__init__.py
@@ -2,31 +2,52 @@
 This is the interface for interacting with the Student Contacts Web Service.
 """
 
-import logging
 import json
 from commonconf import settings
 from uw_sps_contacts.dao import Contacts_DAO
 from restclients_core.exceptions import DataFailureException
-
-logger = logging.getLogger(__name__)
+import logging
 
 
 settings.configure()
-ContactsDao = Contacts_DAO()
+#ContactsDao = Contacts_DAO()
 
 
-def get_resource(url, headers=None):
-    if headers is None:
-        headers = {}
-    response = ContactsDao.getURL(url, headers)
-    logger.info("%s ==status==> %s" % (url, response.status))
+class ContactsList(object):
+    def __init__(self, act_as=None):
+        self.dao = Contacts_DAO()
 
-    if response.status != 200:
-        raise DataFailureException(url, repsonse.status, response.data)
+    def _get_contacts_url(self, syskey):
+        return f"/contacts/v1/emergencyContacts/{syskey}"
 
-    try:
-        logger.debug("%s ==data==> %s" % (url, response.data.decode('utf-8')))
-    except Exception as ex:
-        logger.debug("%s ==Exception==> %s" % (url, ex))
+    def _get_resource(self, syskey, clear_cached_token=False):
+        if clear_cached_token:
+            self.dao.getURL(
+                self._get_contacts_url(syskey), {"Accept": "application/json"})
 
-    return response.data
+    def get_contacts(self, syskey):
+        response = self._get_resource(syskey)
+        if response.status == 200:
+            return self._process_data(json.loads(response.data))
+
+        if response.status == 401 or response.status == 403:
+            # clear token cache, retry
+            response = self._get_resource(syskey, clear_cached_token=True)
+            if response.status == 200:
+                return self._process_data(json.loads(response.data))
+
+        raise DataFailureException(
+            self._get_contacts_url(syskey), response.status, str(response.data))
+
+    def _process_data(self, jdata):
+        print(jdata)  # Implement!!!
+
+#def get_resource(url, headers=None):
+#    if headers is None:
+#        headers = {}
+#    response = ContactsDao.getURL(url, headers)
+#
+#    if response.status != 200:
+#        raise DataFailureException(url, response.status, response.data)
+#
+#    return json.loads(response.data)
--- uw_sps_contacts/dao.py
+++ uw_sps_contacts/dao.py
@@ -2,11 +2,52 @@
 Contains UW Student Contacts DAO implementations.
 """
 from restclients_core.dao import DAO
+import logging
 from os.path import abspath, dirname
 import os
 
+logger = logging.getLogger(__name__)
+sps_contacts_access_token_url = "/oauth2/token"
+
+
+class Contacts_Auth_DAO(DAO):
+    def service_name(self):
+        return 'sps_contacts_auth'
+
+    def _is_cacheable(self, method, url, headers, body=None):
+        return True
+
+    def clear_token_from_cache(self):
+        self.clear_cached_response(sps_contacts_access_token_url)
+
+    def get_auth_token(self, secret):
+        headers = {"Authorization": "Basic {}".format(secret),
+                   "Content-type": "application/x-www-form-urlencoded"}
+
+        response = self.postURL(
+            sps_contacts_access_token_url, headers, "grant_type=client-credentials")
+        if response.status != 200:
+            logger.error({'url': sps_contacts_access_token_url,
+                          'status': response.status,
+                          'data': response.data})
+            raise DataFailureException(
+                sps_contacts_access_token_url, response.status, response.data)
+
+        data = json.loads(response.data)
+        return data.get("access_token", "")
+
+    def service_mock_paths(self):
+        path = [abspath(os.path.join(dirname(__file__), "resources"))]
+        return path
+
+    def _edit_mock_response():
+        pass  # need to implement? check myplan dao
+
 
 class Contacts_DAO(DAO):
+    def __init__(self):
+        self.auth_dao = Contacts_Auth_DAO()
+        return super(Contacts_Auth_DAO, self).__init__()
 
     def service_name(self):
         return 'sps_contacts'
@@ -14,3 +55,12 @@ class Contacts_DAO(DAO):
     def service_mock_paths(self):
         path = [abspath(os.path.join(dirname(__file__), "resources"))]
         return path
+
+    def _custom_headers(self, method, url, headers, body):
+        if not headers:
+            headers = {}
+        secret = self.get_service_setting("AUTH_SECRET", "")
+        if secret:
+            token = self.auth_dao.get_auth_token(secret)
+            headers["Authorization"] = f"Bearer {token}"
+        return headers
