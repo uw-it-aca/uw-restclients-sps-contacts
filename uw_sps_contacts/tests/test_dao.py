511c2750d400c4f5e7467a3c075e44fa9436a23d
Craig M. Stimmel <cstimmel@uw.edu>
2025-11-10 10:56:45 -0800

    Mock auth token

--- uw_sps_contacts/tests/test_dao.py
+++ uw_sps_contacts/tests/test_dao.py
@@ -3,10 +3,18 @@
 
 from unittest import TestCase
 from uw_sps_contacts.dao import Contacts_Auth_DAO
+from uw_sps_contacts.utils import (
+    fdao_sps_contacts_override, fdao_sps_contacts_auth_override)
 
 
+@fdao_sps_contacts_auth_override
+@fdao_sps_contacts_override
 class TestSpsAuth(TestCase):
 
     def test_is_cacheable(self):
         auth = Contacts_Auth_DAO()
         self.assertTrue(auth._is_cacheable("POST", "/", {}, ""))
+
+    def test_get_auth_token(self):
+        self.assertIsNotNone(
+            Contacts_Auth_DAO().get_auth_token("test1"))
---
+++ uw_sps_contacts/utils.py
@@ -0,0 +1,9 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from commonconf import override_settings
+
+fdao_sps_contacts_override = override_settings(
+    RESTCLIENTS_SPS_CONTACTS_DAO_CLASS='Mock')
+fdao_sps_contacts_auth_override = override_settings(
+    RESTCLIENTS_SPS_CONTACTS_AUTH_DAO_CLASS='Mock')
