29a09bb2d1c92e71c92c09063ec79db90aecec01
Jim Laney <jlaney@uw.edu>
2025-11-18 06:31:27 -0800

    refactor dao classes for consistency

--- uw_sps_contacts/dao.py
+++ uw_sps_contacts/dao.py
@@ -8,47 +8,51 @@ from os.path import abspath, dirname
 from restclients_core.dao import DAO
 from restclients_core.exceptions import DataFailureException
 
-from commonconf import settings
-
 logger = logging.getLogger(__name__)
-sps_contacts_access_token_url = "/oauth2/token"
 
 
 class Contacts_Auth_DAO(DAO):
+    sps_contacts_access_token_url = "/oauth2/token"
+
     def service_name(self):
         return "sps_contacts_auth"
 
     def _is_cacheable(self, method, url, headers, body=None):
         return True
 
+    def _custom_headers(self, method, url, headers, body):
+        if not headers:
+            headers = {}
+
+        secret = self.get_service_setting("SECRET")
+
+        headers["Authorization"] = f"Basic {secret}"
+        headers["Content-type"] = "application/x-www-form-urlencoded"
+
+        return headers
+
     def clear_token_from_cache(self):
-        self.clear_cached_response(sps_contacts_access_token_url)
+        self.clear_cached_response(self.sps_contacts_access_token_url)
 
-    def get_auth_token(self, secret):
-        headers = {
-            "Authorization": "Basic {}".format(secret),
-            "Content-type": "application/x-www-form-urlencoded",
-        }
-
+    def get_auth_token(self):
         response = self.postURL(
-            sps_contacts_access_token_url,
-            headers,
-            "grant_type=client_credentials",
+            self.sps_contacts_access_token_url,
+            body="grant_type=client_credentials",
         )
-        if response.status != 200:
-            logger.error(
-                {
-                    "url": sps_contacts_access_token_url,
-                    "status": response.status,
-                    "data": response.data,
-                }
-            )
-            raise DataFailureException(
-                sps_contacts_access_token_url, response.status, response.data
-            )
 
-        data = json.loads(response.data)
-        return data.get("access_token", "")
+        if response.status == 200:
+            data = json.loads(response.data)
+            return data.get("access_token", "")
+
+        # Something bad happened
+        logger.error({
+            "url": self.sps_contacts_access_token_url,
+            "status": response.status,
+            "data": response.data,
+        })
+
+        raise DataFailureException(
+            self.sps_contacts_access_token_url, response.status, response.data)
 
     def service_mock_paths(self):
         path = [abspath(os.path.join(dirname(__file__), "resources"))]
@@ -56,24 +60,22 @@ class Contacts_Auth_DAO(DAO):
 
     def _edit_mock_response(self, method, url, headers, body, response):
         if response.status == 404 and method != "GET":
-            alternative_url = "{0}.{1}".format(url, method)
+            alternative_url = f"{url}.{method}"
             backend = self.get_implementation()
             new_resp = backend.load(method, alternative_url, headers, body)
             response.status = new_resp.status
             response.data = new_resp.data
-            logger.debug(
-                {
-                    "url": alternative_url,
-                    "status": response.status,
-                    "data": response.data,
-                }
-            )
+            logger.debug({
+                "url": alternative_url,
+                "status": response.status,
+                "data": response.data,
+            })
 
 
 class Contacts_DAO(DAO):
     def __init__(self):
         self.auth_dao = Contacts_Auth_DAO()
-        return super(Contacts_DAO, self).__init__()
+        return super().__init__()
 
     def service_name(self):
         return "sps_contacts"
@@ -85,12 +87,10 @@ class Contacts_DAO(DAO):
     def _custom_headers(self, method, url, headers, body):
         if not headers:
             headers = {}
-        secret = self.get_service_setting(
-            "AUTH_SECRET", ""
-        )
-        if secret:
-            token = self.auth_dao.get_auth_token(secret)
-            headers["Authorization"] = f"Bearer {token}"
+
+        token = self.auth_dao.get_auth_token()
+
+        headers["Authorization"] = f"Bearer {token}"
         return headers
 
     def clear_access_token(self):
