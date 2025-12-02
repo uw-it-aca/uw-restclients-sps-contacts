fa8eef173660db784cc957997399cccb689d5771
Craig M. Stimmel <cstimmel@uw.edu>
2025-12-02 11:37:12 -0800

    Use full parent contact information for FamilyContact()

--- uw_sps_contacts/tests/test_family_contact.py
+++ uw_sps_contacts/tests/test_family_contact.py
@@ -35,7 +35,15 @@ class FamilyContactsTest(TestCase):
         contact = fcontacts.get_contact(12345)
 
         self.assertEqual('NAME,PARENT', contact.name)
+        self.assertEqual('C/O STUDENT TEAM', contact.address_line_1)
+        self.assertEqual('UW TOWER O-3 BOX 359565', contact.address_line_2)
+        self.assertEqual('SEATTLE', contact.city)
+        self.assertEqual('WA', contact.state)
+        self.assertEqual('98195', contact.zip_5)
+        self.assertEqual('', contact.zip_filler_b)
         self.assertEqual('2064444444', contact.phone_number)
+        self.assertEqual('', contact.country)
+        self.assertEqual('', contact.postal_cd)
 
         resp = fcontacts._get_resource(12345, clear_cached_token=True)
         self.assertIsNotNone(resp)
@@ -43,10 +51,27 @@ class FamilyContactsTest(TestCase):
     def test_json_data(self):
         contact = FamilyContact()
         contact.name = "Jake Doe"
+        contact.address_line_1 = "C/O STUDENT TEAM"
+        contact.address_line_2 = "UW TOWER O-3 BOX 359565"
+        contact.city = "SEATTLE"
+        contact.state = "WA"
+        contact.zip_5 = "98195"
+        contact.zip_filler_b = ""
         contact.phone_number = "5555551234"
+        contact.country = ""
+        contact.postal_cd = ""
+
         self.assertIsInstance(contact.json_data(), dict)
         string_data = (
-            '{"name": "Jake Doe",'
-            '"phone_number": "5555551234"}'
+            '{"name": "Jake Doe", '
+            '"address_line_1": "C/O STUDENT TEAM", '
+            '"address_line_2": "UW TOWER O-3 BOX 359565", '
+            '"city": "SEATTLE", '
+            '"state": "WA", '
+            '"zip_5": "98195", '
+            '"zip_filler_b": "", '
+            '"phone_number": "5555551234", '
+            '"country": "", '
+            '"postal_cd": ""}'
         )
         self.assertEqual(contact.json_data(), json.loads(string_data))
